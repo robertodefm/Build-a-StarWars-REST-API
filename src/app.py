821ae1c54e56ffb15d38697325b9eb52913afbb0
robertodefm <robertodefm@gmail.com>
2023-08-26 00:00:51 +0000

    completed

--- src/app.py
+++ src/app.py
@@ -8,19 +8,17 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import *
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -36,16 +34,129 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET','POST'])
 def handle_hello():
+    
+    if request.method == 'GET':
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+        all_user = User.query.all()
+        return jsonify(
+            [ users.serialize() for users in all_user]
+        ), 200
+    elif request.method == 'POST':
+        body = request.json
+        if "email" not in body:
+            return 'Debe indicar el email', 400
+        if "password" not in body:
+            return 'Debe indicar una password', 400
+        if "is_active" not in body:
+            return 'Debe indicar is_active', 400
+        new_row = User.new_registro_users(body["email"], body["password"], body["is_active"])
+        if new_row == None:
+            return 'Ha ocurrido un error!', 500
+        else:
+            return jsonify(new_row.serialize()), 200
+    
 
-    return jsonify(response_body), 200
+@app.route('/users/favorites', methods=['GET'])
+def get_users_favorites():
 
-# this only runs if `$ python src/app.py` is executed
+    all_favorites = Favorites.query.all()
+    return jsonify(
+        [ favorites.serialize() for favorites in all_favorites]
+    ), 200
+
+
+
+@app.route('/people', methods=['GET'])
+def people_todos():
+    all_people = People.query.all()
+    return jsonify(
+        [ people.serialize() for people in all_people]
+    ), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def people_search(people_id):
+    people=People.query.get(people_id)
+    if not people:
+        return 'No se encontro registro people con ese id ', 404
+    return jsonify(
+        people.serialize() 
+    ), 200
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST','DELETE'])
+def post_people(people_id):
+    search = People.query.get(people_id)
+
+
+    if request.method == 'POST':
+        body = request.json
+        if "name" not in body:
+            return 'Debe indicar la descripcion porque es favorito!', 400
+        if "user_id" not in body:
+            return 'Debe indicar el usuario', 400
+        
+        new_row = Favorites.new_registro_favorites(body["name"], body["user_id"], None, people_id)
+        if new_row == None:
+            return 'Ha ocurrido un error!!', 500
+        else:
+            return jsonify(new_row.serialize()), 200
+    elif request.method == 'DELETE':
+        searchdelete = Favorites.query.filter_by(people_id=people_id)
+
+        result = searchdelete.delete()
+        db.session.commit()
+        if result == True:
+            return f'El people {people_id} ha sido eliminado con exito!', 200
+        else:
+            return 'Ha ocurrido un error!!', 500
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    all_planets = Planets.query.all()
+    return jsonify(
+        [ planets.serialize() for planets in all_planets]
+    ), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def planet_search(planet_id):
+    planet=Planets.query.get(planet_id)
+    if not planet:
+        return 'No se encontro registro planet con ese id ', 404
+    return jsonify(
+        planet.serialize() 
+    ), 200
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST','DELETE'])
+def post_planets(planet_id):
+    search = Planets.query.get(planet_id)
+
+
+    if request.method == 'POST':
+        body = request.json
+        if "name" not in body:
+            return 'Debe indicar la descripcion porque es favorito!', 400
+        if "user_id" not in body:
+            return 'Debe indicar el usuario', 400
+        
+        new_row = Favorites.new_registro_favorites(body["name"], body["user_id"], search.id, None)
+        if new_row == None:
+            return 'Ha ocurrido un error!', 500
+        else:
+            return jsonify(new_row.serialize()), 200
+    elif request.method == 'DELETE':
+        searchdelete = Favorites.query.filter_by(planet_id=planet_id)
+
+        result = searchdelete.delete()
+        db.session.commit()
+        if result == True:
+            return f'El planeta {planet_id} ha sido eliminado con exito!', 200
+        else:
+            return 'Ha ocurrido un error!!', 500
+
+
+
+# this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,22 +1,3 @@
-# from flask_sqlalchemy import SQLAlchemy
-
-# db = SQLAlchemy()
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     password = db.Column(db.String(80), unique=False, nullable=False)
-#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "email": self.email,
-#             # do not serialize the password, its a security breach
-#         }
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -27,10 +8,10 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    favorites = db.relationship("Favorite", backref="user", uselist=True)
+    favorites = db.relationship("Favorites", backref="user", uselist=True)
     
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -38,6 +19,18 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    @classmethod
+    def new_registro_users(cls, email, password, is_active):
+        new_registro_users = cls( email, password, is_active)
+       
+        db.session.add(new_registro_users)
+        try:
+            db.session.commit()
+            return new_registro_users
+        except Exception as error:
+            print(error)
+            return None
         
 class People(db.Model):
     id = db.Column(db.Integer,primary_key=True)  
@@ -48,49 +41,105 @@ class People(db.Model):
     def serialize(self):
         return{
             "id": self.id,
-            'uid': self.uid,
             "name": self.name,
             "url_address": self.url_address,
             
         }
+
+    @classmethod
+    def new_registro_people(cls, uid, name, url_address):
+        new_registro_people = cls( uid, name, url_address)
+       
+        db.session.add(new_registro_people)
+        try:
+            db.session.commit()
+            return new_registro_people
+        except Exception as error:
+            print(error)
+            return None
         
 class Planets(db.Model):
     id = db.Column(db.Integer,primary_key=True)  
     name = db.Column(db.String(100), nullable=False)
     info = db.Column(db.String(400), nullable=False)
-    favorite_planet_id = db.Column(db.Integer,nullable=False )
+    
     def serialize(self):
         return{
             "id": self.id,
             "name": self.name,
             "info": self.info,
-        }     
+        }    
+
+    @classmethod
+    def new_registro_planets(cls, name, info):
+        new_registro_planets = cls( name, info)
+       
+        db.session.add(new_registro_planets)
+        try:
+            db.session.commit()
+            return new_registro_planets
+        except Exception as error:
+            print(error)
+            return None    
+     
         
-class Favorite(db.Model):
+class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    url_address = db.Column(db.String(300), nullable=False)
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    people_id = db.Column(db.Integer,db.ForeignKey('people.id'))
+    planet_id = db.Column(db.Integer,db.ForeignKey('planets.id'))
     __table_args__ =(db.UniqueConstraint(
         'user_id', 
-        'url_address',
-        name = 'unique_favorite'
+        'people_id',
+        name = 'unique_favorites'
     ),)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.name
+
+    def __init__(self, name, user_id, planet_id, people_id):
+        self.name = name
+        self.user_id = user_id
+        self.planet_id = planet_id
+        self.people_id = people_id
 
     def serialize(self):
         return{
            "id": self.id,
            "user_id": self.user_id,
-           "url_address": self.url_address,
-           'favorite_name': self.name,  
+           "people_id": self.people_id,
+           "planet_id": self.planet_id,
+             
            
         }
         
+    @classmethod
+    def new_registro_favorites(cls, name, user_id, planet_id, people_id):
+        new_registro_favorites = cls(name, user_id, planet_id, people_id)
+       
+        db.session.add(new_registro_favorites)
+        try:
+            db.session.commit()
+            return new_registro_favorites
+        except Exception as error:
+            print(error)
+            return None
+
+    def update(self, name):
+        self.name = name
+        try:
+            db.session.commit()
+            return self
+        except Exception as error:
+            print(error)
+            return False
+
     def delete(self):
         db.session.delete(self)
         try:
             db.session.commit()
             return True
         except Exception as error:
-            db.session.rollback()
-            return False     
+            print(error)
+            return False
